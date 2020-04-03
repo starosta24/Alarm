d04fd264f89a481acf15d33c191edab9e2793049
starosta24 <62113035+starosta24@users.noreply.github.com>
2020-04-03 15:55:01 +0300

    Добавьте файлы проекта.

---
+++ Alarms/Alarms.Android/MainActivity.cs
@@ -0,0 +1,74 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Util;
+using Android.Text.Format;
+using Alarms.Droid;
+
+namespace TimePickerDemo
+{
+    [Activity(Label = "Будильник" , MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        TextView timeDisplay;
+        Button timeSelectButton;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            var tDisp = "@time_display";
+            var but = "@select_button";
+            timeDisplay = FindViewById<TextView>(Resource.Id.time_display);
+            timeSelectButton = FindViewById<Button>(Resource.Id.select_button);
+            
+            timeSelectButton.Click += TimeSelectOnClick;
+        }
+
+        void TimeSelectOnClick(object sender, EventArgs eventArgs)
+        {
+            TimePickerFragment frag = TimePickerFragment.NewInstance(
+                delegate (DateTime time)
+                {
+                    timeDisplay.Text = time.ToShortTimeString();
+                });
+
+            frag.Show(FragmentManager, TimePickerFragment.TAG);
+        }
+    }
+
+
+
+    public class TimePickerFragment : DialogFragment, TimePickerDialog.IOnTimeSetListener
+    {
+        public static readonly string TAG = "MyTimePickerFragment";
+        Action<DateTime> timeSelectedHandler = delegate { };
+
+        public static TimePickerFragment NewInstance(Action<DateTime> onTimeSelected)
+        {
+            TimePickerFragment frag = new TimePickerFragment();
+            frag.timeSelectedHandler = onTimeSelected;
+            return frag;
+        }
+
+        public override Dialog OnCreateDialog(Bundle savedInstanceState)
+        {
+            DateTime currentTime = DateTime.Now;
+            bool is24HourFormat = DateFormat.Is24HourFormat(Activity);
+            TimePickerDialog dialog = new TimePickerDialog
+                (Activity, this, currentTime.Hour, currentTime.Minute, is24HourFormat);
+            return dialog;
+        }
+
+        public void OnTimeSet(TimePicker view, int hourOfDay, int minute)
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime selectedTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, hourOfDay, minute, 0);
+            Log.Debug(TAG, selectedTime.ToLongTimeString());
+            timeSelectedHandler(selectedTime);
+        }
+
+
+    }
+}
